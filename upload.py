f0387c5f26fa2a789a737d17a4f81a7d4748607e
szachovy <wjmaj98@gmail.com>
2019-11-22 19:57:31 +0100

    basic file upload using flask with success response

--- upload.py
+++ upload.py
@@ -1,9 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-if __name__ == "__main__":
-    app.run()
+from flask import *  
+import pandas as pd
+app = Flask(__name__)  
+     
+@app.route('/')  
+def upload():  
+    return render_template("upload.html")  
+     
+@app.route('/describe', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
+        f = request.files['file']  
+        # f.save(f.filename)
+        return render_template("describe.html", name = pd.read_csv(f))  
+      
+if __name__ == '__main__':  
+    app.run(debug = True)  
